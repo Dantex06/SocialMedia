48ab0d75bde3421dea77d05c4a234f9e48ee2955
Egor <egorrogachev030@gmail.com>
2024-07-25 01:29:09 +0300

    fix bugs with mobx in user and news pages

--- src/entities/posts/store/posts-store.ts
+++ src/entities/posts/store/posts-store.ts
@@ -1,5 +1,5 @@
-import { postsGet, postsGetMore, userPosts } from '@/shared/api/auth';
-import { makeAutoObservable } from 'mobx';
+import { PostsGet, PostsGetMore, UserPosts } from '@/shared/api/auth';
+import { action, computed, makeObservable, observable } from 'mobx';
 import { AxiosError } from 'axios';
 
 export interface IPostData {
@@ -18,10 +18,10 @@ export interface IPostData {
 }
 
 export interface PostUserState {
-    "first": number | null,
-    "current": number | null,
-    "last": number | null,
-    "posts": IPostData[];
+    first: number | null;
+    current: number | null;
+    last: number | null;
+    posts: IPostData[];
 }
 
 export interface PostsState {
@@ -31,17 +31,37 @@ export interface PostsState {
     posts: IPostData[];
 }
 
-class PostsStore {
+class PostsCounter {
+    private _pagePosts: number = 1;
+    private _errorPosts: null | AxiosError = null;
+
+    get error(): AxiosError | null {
+        return this._errorPosts;
+    }
+
+    set setCountPage(countPage: number) {
+        this._pagePosts = countPage;
+    }
+
+    get countPage() {
+        return this._pagePosts;
+    }
+
+    updatePage = () => {
+        this._pagePosts += 1;
+    };
+}
+
+class PostsStore extends PostsCounter {
     public loading: boolean = false;
-    private _page: number = 1;
     private _error: null | AxiosError = null;
 
     private _postsUserData: PostUserState = {
         first: 1,
         current: 1,
         last: 52,
-        posts: []
-    }
+        posts: [],
+    };
 
     private _postsData: PostsState = {
         first: 1,
@@ -50,19 +70,11 @@ class PostsStore {
         posts: [],
     };
 
-    get page(){
-        return this._page;
-    }
-
-    updatePage = () => {
-        this._page += 1;
-    }
-
-    set updatePosts({ current, first, last, posts }: PostsState){
+    set updatePosts({ current, first, last, posts }: PostsState) {
         this._postsData.first = first;
         this._postsData.last = last;
         this._postsData.current = current;
-        posts.forEach((post)=>{this._postsData.posts.push(post)});
+        this._postsData.posts.push(...posts);
     }
 
     get error(): AxiosError | null {
@@ -80,58 +92,51 @@ class PostsStore {
     getUserPosts = (id: number): Promise<void> => {
         this._error = null;
         this.loading = true;
-        return userPosts(id)
-         .then((response)=> {
-             if (response.data) {
-                 const { current, first, last, posts } = response.data;
-                 this._postsUserData = {
-                     current,
-                     first,
-                     last,
-                     posts,
-                 };
-             }
-         }).catch((error) => {
-             this._error = error;
-         }).finally(()=>{
-             this.loading = false;
-         })
-    }
-
-    getPostsMore = (): Promise<void> => {
-        this._error = null;
-        return postsGetMore(this.page)
-         .then((response) => {
-             if (response.data ) {
-                 console.log(response.status)
-                 const { current, first, last, posts } = response.data;
-                 this.updatePosts = { first, current, last, posts };
-                 console.log(posts.length);
-                 this._error = null;
-                 this.updatePage();
-             }
-         })
-         .catch((error) => {
-             this._error = error.response.statusText;
-         })
-    };
-
-    getPosts = (): Promise<void> => {
-        this.loading = true;
-        this._error = null;
-        this._page = 1;
-        return postsGet()
+        return UserPosts(id)
             .then((response) => {
                 if (response.data) {
                     const { current, first, last, posts } = response.data;
-                    this._postsData = {
+                    this._postsUserData = {
                         current,
                         first,
                         last,
                         posts,
                     };
-                    this._error = null;
-                    this.loading = false;
+                }
+            })
+            .catch((error) => {
+                this._error = error;
+            })
+            .finally(() => {
+                this.loading = false;
+            });
+    };
+
+    getPostsMore = (): Promise<void>  => {
+        this._error = null;
+        return PostsGetMore(super.countPage)
+            .then((response) => {
+                if (response.data) {
+                    const { current, first, last, posts } = response.data;
+                    this.updatePosts = { first, current, last, posts };
+                    this.updatePage();
+                }
+            })
+            .catch((error) => {
+                this._error = error.response.statusText;
+            });
+    };
+
+    getPosts = (): Promise<void> => {
+        this.loading = true;
+        this._error = null;
+        this._postsData.posts = [];
+        super.setCountPage = 1;
+        return PostsGet()
+            .then((response) => {
+                if (response.data) {
+                    const { current, first, last, posts } = response.data;
+                    this.updatePosts = { current, first, last, posts };
                     this.updatePage();
                 }
             })
@@ -144,7 +149,16 @@ class PostsStore {
     };
 
     constructor() {
-        makeAutoObservable(this);
+        super();
+        makeObservable<PostsStore, "_postsData" | "_postsUserData">(this, {
+            loading: observable,
+            error: computed,
+            _postsData: observable,
+            _postsUserData: observable,
+            getUserPosts: action,
+            getPostsMore: action,
+            getPosts: action,
+        });
     }
 }
 
--- src/pages/news-page/ui/NewsPage.tsx
+++ src/pages/news-page/ui/NewsPage.tsx
@@ -9,6 +9,7 @@ export const NewsPage = observer(() => {
     const {
         postsStore: { getPosts, error, loading, posts, getPostsMore },
     } = useStores();
+
     const storedProfile = window.localStorage.getItem('profile_id');
     const userData = storedProfile ? JSON.parse(storedProfile) : null;
     useEffect(() => {
@@ -17,41 +18,37 @@ export const NewsPage = observer(() => {
 
     const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
         const target = e.currentTarget;
-        if (target.scrollHeight - target.scrollTop <= target.clientHeight + 50) { // 50px для буфера
-            if(posts.posts.length%100===0){
+        if (target.scrollHeight - target.scrollTop <= target.clientHeight + 50) {
+            if (posts.posts.length % 100 === 0) {
                 getPostsMore();
             }
         }
-    }
-
+    };
 
     if (loading) {
         return <Loading />;
     }
 
     if (error) {
-        console.log(error);
         return <div>{error.code}</div>;
     }
-    if (!loading && !error) {
-        return (
-            <div className={cls.posts} onScroll={handleScroll}>
-                {posts.posts.map((post, num) => (
-                    <Post
-                        key={num}
-                        id={post.id}
-                        isLiked={post.is_liked}
-                        likesCount={post.likes_count}
-                        name={post.author.name}
-                        surname={post.author.surname}
-                        text={post.content}
-                        photo={null}
-                        published={post.published_at}
-                        idUser={post.author.id}
-                        myId={userData ? userData.id : null}
-                    />
-                ))}
-            </div>
-        );
-    }
+    return (
+        <div className={cls.posts} onScroll={handleScroll}>
+            {posts.posts.map((post, num) => (
+                <Post
+                    key={num}
+                    id={post.id}
+                    isLiked={post.is_liked}
+                    likesCount={post.likes_count}
+                    name={post.author.name}
+                    surname={post.author.surname}
+                    text={post.content}
+                    photo={null}
+                    published={post.published_at}
+                    idUser={post.author.id}
+                    myId={userData ? userData.id : null}
+                />
+            ))}
+        </div>
+    );
 });
